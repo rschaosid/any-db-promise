d57047e9aa11efaf11350216a5c28d6c0b430e11
rschaosid <rschaosid@gmail.com>
2014-07-28 23:36:04 -0600

    rc

---
+++ index.js
@@ -0,0 +1,22 @@
+var anyDB = require('any-db');
+var Promise = require('promise');
+
+exports.createConnection = function() {
+  return Promise.denodeify(anyDB.createConnection).apply(arguments)
+  .then(function(conn) {
+    conn.queryStream = conn.query;
+    conn.query = Promise.nodeify(Promise.denodeify(conn.query));
+    conn.end = Promise.nodeify(Promise.denodeify(conn.end));
+    return conn;
+  });
+}
+
+exports.createPool = function() {
+  var pool = anyDB.createPool.apply(arguments);
+  
+  pool.queryStream = pool.query;
+  pool.query = Promise.nodeify(Promise.denodeify(pool.query));
+  pool.close = Promise.nodeify(Promise.denodeify(pool.close));
+  
+  return pool;
+};
---
+++ transaction/index.js
@@ -0,0 +1,13 @@
+var transaction = require('any-db-transaction');
+var Promise = require('promise');
+
+module.exports = function begin() {
+  return Promise.denodeify(transaction).apply(arguments)
+  .then(function(tx) {
+    tx.queryStream = tx.query;
+    tx.query = Promise.nodeify(Promise.denodeify(tx.query));
+    tx.commit = Promise.nodeify(Promise.denodeify(tx.commit));
+    tx.rollback = Promise.nodeify(Promise.denodeify(tx.rollback));
+    return conn;
+  });
+};
