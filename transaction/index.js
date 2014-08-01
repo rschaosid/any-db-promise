07947038c5543d6f028071bdd5af46dab78fc557
rschaosid <rschaosid@gmail.com>
2014-08-01 02:04:00 -0600

    FSM is filthy and I hate you for creating/using it

--- transaction/index.js
+++ transaction/index.js
@@ -4,10 +4,10 @@ var Promise = require('promise');
 module.exports = function begin() {
   return Promise.denodeify(transaction).apply(undefined, arguments)
   .then(function(tx) {
-    tx.queryStream = tx.query;
-    tx.query = Promise.nodeify(Promise.denodeify(tx.query));
-    tx.commit = Promise.nodeify(Promise.denodeify(tx.commit));
-    tx.rollback = Promise.nodeify(Promise.denodeify(tx.rollback));
-    return conn;
+    return {
+      query: Promise.nodeify(Promise.denodeify(tx.query.bind(tx))),
+      commit: Promise.nodeify(Promise.denodeify(tx.commit.bind(tx))),
+      rollback: Promise.nodeify(Promise.denodeify(tx.rollback.bind(tx)))
+    };
   });
 };
